849c468b90ac940c2e59309fb657ed58d459c968
Sarathkumar Ravichandran <161054832+SarathKumar1606@users.noreply.github.com>
2026-06-09 20:19:13 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    HashMap<Integer,Integer> mp = new HashMap<>();
+    private int dp(int i,int n){
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(mp.containsKey(i))
+            return mp.get(i);
+        int res=dp(i+1,n)+dp(i+2,n);
+        mp.put(i,res);
+        return res;
+    }
+    public int climbStairs(int n) {
+        return dp(0,n);
+    }
+}
